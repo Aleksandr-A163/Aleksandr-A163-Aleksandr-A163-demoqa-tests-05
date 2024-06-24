5a6df006d8f7f04c2f6bab15262c78dd1f48ab98
Aleksandr Anosov <you@example.com>
2024-06-24 20:58:42 +0300

    updated SolutionEnterpriseTest

--- src/test/java/SolutionsEnterpriseTest.java
+++ src/test/java/SolutionsEnterpriseTest.java
@@ -1,30 +1,26 @@
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
 
 import static com.codeborne.selenide.Selenide.*;
-
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.WebDriverConditions.url;
 
 public class SolutionsEnterpriseTest {
 
     @BeforeAll
     static void setUp() {
-        Configuration.baseUrl = "https://github.com";
+        Configuration.baseUrl = "https://github.com/";
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
     }
 
-    @AfterEach
-    void tearDown(){
-        closeWebDriver();
-    }
-
     @Test
         void searchEnterprise() {
-        open("/selenide/selenide");
-
+        open("https://github.com/");
+        $(byTagAndText("button", "Solutions")).hover();
+        $(byTagAndText("a", "Enterprise")).click();
+        webdriver().shouldHave(url("https://github.com/enterprise"));
     }
 }
